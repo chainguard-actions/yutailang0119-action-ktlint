65c6f3bad3d05e0e9279c42ed3801384afaea09e
actionchain <actionchain@chainguard.dev>
2026-06-22 23:20:08 +0000

    Update yutailang0119/action-ktlint@v3.0.0

---
+++ src/parser.ts
@@ -0,0 +1,43 @@
+import fs from 'fs'
+import * as core from '@actions/core'
+import * as xml2js from 'xml2js'
+import {Annotation} from './annotation'
+
+export const parseXmls = async (files: string[]): Promise<Annotation[]> => {
+  const list = await Promise.all(
+    files.map(async file => {
+      const xml = fs.readFileSync(file, 'utf-8')
+      return await parseXml(xml)
+    })
+  )
+  return list.flat()
+}
+
+export const parseXml = async (text: string): Promise<Annotation[]> => {
+  const parser = new xml2js.Parser()
+  const xml = await parser.parseStringPromise(text)
+  return new Promise(resolve => {
+    try {
+      const annotations: Annotation[] = []
+      for (const fileElement of xml.checkstyle.file) {
+        const file = fileElement.$
+
+        for (const errorElement of fileElement.error) {
+          const error = errorElement.$
+
+          const annotation = new Annotation(
+            error.severity,
+            error.message,
+            file.name,
+            parseInt(error.line),
+            parseInt(error.column)
+          )
+          annotations.push(annotation)
+        }
+      }
+      resolve(annotations)
+    } catch (error) {
+      core.debug(`failed to read ${error}`)
+    }
+  })
+}
